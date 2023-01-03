1a84a24102260ce104d868806969085a907863f6
weng.jiankai <weng.jiankai@iwhalecloud.com>
2023-01-03 14:26:48 +0800

    feat: 00011-easy-tuple-to-object

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -1,1 +1,13 @@
-type TupleToObject<T extends readonly any[]> = any
+type TupleToObject<T extends readonly (string | number | symbol)[]> = {
+  [K in T[number]]: K
+}
+
+const tuple1 = [2, 'bar', Symbol('')]
+
+type Case1 = typeof tuple1 // (string | number | symbol)[]
+type Case2 = TupleToObject<Case1>
+// {
+//   [x: string]: string;
+//   [x: number]: number;
+//   [x: symbol]: symbol;
+// }
