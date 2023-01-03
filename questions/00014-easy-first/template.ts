13f33854c8c131609f2a998fa2779d13d605194e
weng.jiankai <weng.jiankai@iwhalecloud.com>
2023-01-03 14:35:51 +0800

    feat: 00014-easy-first

--- questions/00014-easy-first/template.ts
+++ questions/00014-easy-first/template.ts
@@ -1,1 +1,4 @@
-type First<T extends any[]> = any
+// type First<T extends any[]> = T extends [] ? never : T[0]
+type First<T extends any[]> = T extends [infer First, ...any] ? First : never
+
+type Case = First<[3, 2, 1]>
