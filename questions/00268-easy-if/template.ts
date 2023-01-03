aa623e3fd470528deb6ae57aec791186bfe900ae
weng.jiankai <weng.jiankai@iwhalecloud.com>
2023-01-03 15:18:03 +0800

    feat: 00268-easy-if

--- questions/00268-easy-if/template.ts
+++ questions/00268-easy-if/template.ts
@@ -1,1 +1,6 @@
-type If<C, T, F> = any
+type If<C extends boolean, T, F> = C extends true ? T : F
+
+// type T2<C extends boolean> = C extends true ? 1 : 2
+// type Q5 = T2<boolean> // 1 | 2，extends 左边是联合类型，返回的结果也是联合类型，触发了 distributive
+// type Q6 = T2<true> // 1
+// type Q7 = T2<false> // 2
